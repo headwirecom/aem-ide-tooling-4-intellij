0a0a5aac98d36cded85be420518be167fafa773f
Andreas Schaefer <schaefera@me.com>
2015-04-30 07:44:22 -0700

    Adjustments of the Server Configuration Dialog Handling and Storage
    Signed-off-by: Andreas Schaefer <schaefera@me.com>

--- src/main/java/com/headwire/aem/tooling/intellij/ui/ServerConfigurationDialog.java
+++ src/main/java/com/headwire/aem/tooling/intellij/ui/ServerConfigurationDialog.java
@@ -5,16 +5,14 @@ import javax.swing.*;
 import com.headwire.aem.tooling.intellij.config.ServerConfiguration;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.*;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 public class ServerConfigurationDialog
         extends DialogWrapper
 {
     private JPanel contentPane;
-    private JTextField hostName;
-    private JTextField configurationPath;
-    private JComboBox runtimeEnvironment;
-    private JTextField configurationName;
+    private JTextField host;
     private JTabbedPane tabbedPane1;
     private JTextField connectionDebugPort;
     private JTextField connectionUserName;
@@ -29,29 +27,55 @@ public class ServerConfigurationDialog
     private JRadioButton installBundlesViaBundleRadioButton;
     private JRadioButton installBundlesDirectlyFromRadioButton;
     private JButton installButton;
-    private TextFieldWithBrowseButton workDirectoryField;
+    private JTextField name;
+    private JTextField description;
 
     public ServerConfigurationDialog(@Nullable Project project) {
         super(project);
 
-        setTitle("Edit Server Properties");
+        setTitle("Create Server Connection Properties");
         setModal(true);
-        setUpDialog();
+        setUpDialog(null);
+        init();
+    }
+
+    public ServerConfigurationDialog(@Nullable Project project, @Nullable ServerConfiguration serverConfiguration) {
+        super(project);
+
+        setTitle("Edit Server Connection Properties");
+        setModal(true);
+        setUpDialog(serverConfiguration);
         init();
     }
 
     public ServerConfiguration getConfiguration() {
         ServerConfiguration ret = new ServerConfiguration();
+        ret.setName(name.getText());
+        ret.setHost(host.getText());
+        ret.setDescription(description.getText());
+        ret.setConnectionPort(obtainInteger(connectionPort, 0));
+        ret.setDebugConnectionPort(obtainInteger(connectionDebugPort, 0));
+        ret.setUserName(connectionUserName.getText());
+        ret.setPassword(connectionPassword.getPassword());
+        ret.setContextPath(connectionContextPath.getText());
+        ret.setStartConnectionTimeoutInSeconds(obtainInteger(startConnectionTimeout, -1));
+        ret.setStopConnectionTimeoutInSeconds(obtainInteger(stopConnectionTimeout, -1));
+
+        
+        ret.setName(name.getText());
+        ret.setName(name.getText());
+        ret.setName(name.getText());
         ret.setServerName(configurationName.getText());
-        ret.setHostName(hostName.getText());
+        ret.setHostName(host.getText());
         ret.setRuntimeEnvironment(runtimeEnvironment.getSelectedIndex());
         ret.setConfigurationPath(configurationPath.getText());
 
         return ret;
     }
 
-    private void setUpDialog() {
-
+    private void setUpDialog(ServerConfiguration serverConfiguration) {
+//        if(serverConfiguration != null) {
+//            name.setText(serverConfiguration.);
     }
 
 //
@@ -71,5 +95,35 @@ public class ServerConfigurationDialog
     @Override
     protected JComponent createCenterPanel() {
         return contentPane;
+    }
+
+    private int obtainInteger(JTextField textField, int defaultValue) {
+        int ret = defaultValue;
+        if(textField != null) {
+            String value = textField.getText();
+            if(StringUtils.isNotBlank(value)) {
+                try {
+                    ret = Integer.parseInt(value);
+                } catch(NumberFormatException e) {
+                    // Ignore
+                }
+            }
+        }
+        return ret;
+    }
+
+    private int obtainInteger(JSpinner spinner, int defaultValue) {
+        int ret = defaultValue;
+        if(spinner != null) {
+            String value = spinner.getValue() + "";
+            if(StringUtils.isNotBlank(value)) {
+                try {
+                    ret = Integer.parseInt(value);
+                } catch(NumberFormatException e) {
+                    // Ignore
+                }
+            }
+        }
+        return ret;
     }
 }
